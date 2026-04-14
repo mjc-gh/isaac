4ce53f31cd2d5d6f5d011877ed7014b990af8f69
Michael Coyne <mjc@hey.com>
2026-04-14 07:27:57 -0400

    Updates to env and cred management

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,18 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Setup STMP configuration for ActionMailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              Environ["SMTP_ADDRESS"],
+    port:                 587,
+    domain:               Environ["SMTP_DOMAIN"],
+    user_name:            Environ["SMTP_USER_NAME"],
+    password:             Environ["SMTP_PASSWORD"],
+    authentication:       "plain",
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5
+  }
 end
--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,9 +1,6 @@
 RubyLLM.configure do |config|
   # Set API key
-  config.openrouter_api_key = ENV.fetch(
-    "OPENROUTER_API_KEY",
-    Rails.application.credentials.dig(:openrouter_api_key)
-  )
+  config.openrouter_api_key = Environ["OPENROUTER_API_KEY"]
 
   config.default_model = "google/gemma-3-12b-it:free"
 
