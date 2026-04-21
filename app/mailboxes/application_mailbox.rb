10342a792c9a3d29b24b117a0a2f216531f572c6
Michael Coyne <mjc@hey.com>
2026-04-21 11:49:09 +0000

    Remove nocov comments from app mailbox

--- app/mailboxes/application_mailbox.rb
+++ app/mailboxes/application_mailbox.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-# :nocov:
 class ApplicationMailbox < ActionMailbox::Base
   routing ->(mail) { mail.to.include?(Environ["SMTP_USER_NAME"]) } => :assistant
 end
-# :nocov:
