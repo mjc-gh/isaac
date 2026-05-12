e8d7805f56287ffffdfaabf896ab1af35b0f43ba
Michael Coyne <mjc@hey.com>
2026-05-12 12:50:50 +0000

    Minor fixes for github CI

--- env.example.sh
+++ env.example.sh
@@ -1,10 +1,14 @@
 # For local development the following is required
+export ISAAC_GOOGLE_CLIENT_ID=client_id
+export ISAAC_GOOGLE_CLIENT_SECRET=client_secret
 export ISAAC_OPENROUTER_API_KEY=openrouter_api_key
 export ISAAC_POSTMARK_API_KEY=postmark_api_key
 export ISAAC_SENDER_ADDRESS=isaac@example.com
+
 export ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY=primary_key
 export ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY=deterministic_key
 export ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT=derivation_salt
+
 export SECRET_KEY_BASE=secret_key_base
 
 # For deploys with Kamal
